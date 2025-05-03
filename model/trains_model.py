40cb357b11047a1115035f63e9ed48cf409f5ba4
oren51 <orenorenoren@gmail.com>
2025-05-03 10:32:25 +0300

    full models

--- model/trains_model.py
+++ model/trains_model.py
@@ -33,3 +33,52 @@ class Trains:
             cursor.close()
         return rows
     
+
+    @staticmethod
+    def get_one_by_id_trains(id):
+        with sqlite3.connect("db/trainsDB.db") as connection:
+            cursor=connection.cursor()
+            sql="""SELECT 
+                trains.id, 
+                trains.name, 
+                trains.engine, 
+                trains.speed, 
+                trains.route, 
+                trains.type, 
+                trains.line_id,
+                lines.name,
+                lines.status
+                FROM trains INNER JOIN lines ON trains.line_id=lines.id
+                WHERE trains.id=?"""
+            cursor.execute(sql,(id,))
+            row=cursor.fetchone()
+            cursor.close()
+        return row
+    
+
+    @staticmethod
+    def update_speed_by_id_trains(id,speed):
+        with sqlite3.connect("db/trainsDB.db") as connection:
+            cursor=connection.cursor()
+            sql="""UPDATE trains
+                SET speed=?
+                WHERE id=?"""
+            cursor.execute(sql,(speed,id))
+            last_update=cursor.lastrowid
+            cursor.close()
+        return f"Train ID: {last_update} / Status: Updated."
+
+
+    @staticmethod
+    def delete_by_id_trains(id):
+        with sqlite3.connect("db/trainsDB.db") as connection:
+            cursor=connection.cursor()
+            sql="""DELETE FROM trains
+                WHERE id=?"""
+            cursor.execute(sql,(id,))
+            last_delete=cursor.lastrowid
+            cursor.close()
+        return f"Train ID: {last_delete} / Status: Deleted."
+    
+
+    
