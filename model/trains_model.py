9e9bdb0cb61684ea7260ad2cf905472dafc9c2e4
oren51 <orenorenoren@gmail.com>
2025-05-03 09:15:46 +0300

    new version

--- model/trains_model.py
+++ model/trains_model.py
@@ -3,22 +3,33 @@ import sqlite3
 class Trains:
 
     @staticmethod
-    def insert_trains (name,engine,speed,route,type,line_id):
+    def get_all_trains():
         with sqlite3.connect("db/trainsDB.db") as connection:
             cursor=connection.cursor()
-            sql="""INSERT INTO trains (
-                name,
-                engine,
-                speed,
-                route,
-                type,
-                line_id) 
-                VALUES (?,?,?,?,?,?)"""
-            cursor.execute(sql,(name,engine,speed,route,type,line_id))
-            last_train=cursor.lastrowid
+            sql="""SELECT 
+            trains.id, 
+            trains.name, 
+            trains.engine, 
+            trains.speed, 
+            trains.route, 
+            trains.type, 
+            trains.line_id,
+            lines.name,
+            lines.status
+            FROM trains INNER JOIN lines ON trains.line_id=lines.id"""
+            cursor.execute(sql)
+            rows=cursor.fetchall()
             cursor.close()
-        return f"Train {last_train} has been inserted."
+        return rows
     
-    
-    
+
+    @staticmethod
+    def get_all_lines():
+        with sqlite3.connect("db/trainsDB.db") as connection:
+            cursor=connection.cursor()
+            sql="""SELECT * FROM lines"""
+            cursor.execute(sql)
+            rows=cursor.fetchall()
+            cursor.close()
+        return rows
     
