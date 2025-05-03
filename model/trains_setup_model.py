a04f82670b83f88b4f10327d101189abec7b5a0d
oren51 <orenorenoren@gmail.com>
2025-05-03 08:39:09 +0300

    version3 for model

--- model/trains_setup_model.py
+++ model/trains_setup_model.py
@@ -2,6 +2,7 @@ import sqlite3
 
 class TrainsSetup:
 
+
     @staticmethod
     def create_table_trains():
         with sqlite3.connect("db/trainsDB.db") as connection:
@@ -19,29 +20,48 @@ class TrainsSetup:
         return "Trains table has been created."
 
 
-@staticmethod
-def create_table_lines():
-    with sqlite3.connect("db/trainsDB.db") as connection:
-        cursor=connection.cursor()
-        sql="""CREATE TABLE IF NOT EXISTS lines (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT,
-            status TEXT)"""
-        cursor.execute(sql)
-        cursor.close()
-    return "Lines table has been created."
+    @staticmethod
+    def insert_trains (name,engine,speed,route,type,line_id):
+        with sqlite3.connect("db/trainsDB.db") as connection:
+            cursor=connection.cursor()
+            sql="""INSERT INTO trains (
+                name,
+                engine,
+                speed,
+                route,
+                type,
+                line_id) 
+                VALUES (?,?,?,?,?,?)"""
+            cursor.execute(sql,(name,engine,speed,route,type,line_id))
+            last_train=cursor.lastrowid
+            cursor.close()
+        return f"Train {last_train} has been inserted."
 
 
-@staticmethod
-def insert_lines(name,status):
-    with sqlite3.connect("db/trainsDB.db") as connection:
-        cursor=connection.cursor()
-        sql="""INSERT INTO lines (
-        name,
-        status) 
-        VALUES (?,?)"""
-        cursor.execute(sql,(name,status))
-        last_line=cursor.lastrowid
-        cursor.close()
-    return f"Line {last_line} has been inserted."
+    @staticmethod
+    def create_table_lines():
+        with sqlite3.connect("db/trainsDB.db") as connection:
+            cursor=connection.cursor()
+            sql="""CREATE TABLE IF NOT EXISTS lines (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                status TEXT)"""
+            cursor.execute(sql)
+            cursor.close()
+        return "Lines table has been created."
+
+
+    @staticmethod
+    def insert_lines(name,status):
+        with sqlite3.connect("db/trainsDB.db") as connection:
+            cursor=connection.cursor()
+            sql="""INSERT INTO lines (
+            name,
+            status) 
+            VALUES (?,?)"""
+            cursor.execute(sql,(name,status))
+            last_line=cursor.lastrowid
+            cursor.close()
+        return f"Line {last_line} has been inserted."
+
 
